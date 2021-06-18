47c1faedd03a9ef79f767a32cd110ecdfe2fa8e9
Darya6918 <85610890+Darya6918@users.noreply.github.com>
2021-06-18 13:01:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import math
+import random
+
+# 3
+# Написать мини калькулятор.
+# В консоле ожидается ввод того сивола, операцию которую мы будем выполнять.
+# Операцции: +, -, /, *, Возведение в степень, модуль, рандомное число, факториал и пусть будет арккосинус.
+# Далее в консоль вводится столько значений, сколько требуется для операции. Для рандомного например 0.
+# И выводим значение.
+
+operator = input("ввести арифметический оператор: ")
+if operator == "+":
+    a = float(input("ввести значение a: "))
+    b = float(input("ввести значение b: "))
+    print(a + b)
+elif operator == "-":
+    a = float(input("ввести значение a:  "))
+    b = float(input("ввести значение b: "))
+    print(a - b)
+elif operator == "/":
+    a = float(input("ввести значение a:  "))
+    b = float(input("ввести значение b: "))
+    print(a / b)
+elif operator == "*":
+    a = float(input("ввести значение a:  "))
+    b = float(input("ввести значение b: "))
+    print(a * b)
+elif operator == "pow":
+    a = float(input("ввести значение a: "))
+    b = float(input("ввести значение b: "))
+    print(pow(a, b))
+elif operator == "module":
+    a = float(input("ввести значение: "))
+    print(abs(a))
+elif operator == "div":
+    a = float(input("ввести значение a:  "))
+    b = float(input("ввести значение b: "))
+    print(a // b)
+elif operator == "mod":
+    a = float(input("ввести значение a:  "))
+    b = float(input("ввести значение b: "))
+    print(a % b)
+elif operator == "f":
+    a = int(input("ввести  значение: "))
+    print(math.factorial(a))
+elif operator == "arccos":
+    a = float(input("ввести значение: "))
+    b = a * math.pi / 180
+    print(math.acos(b))
+elif operator == "0":  # операция рандомное число, если нажать на ноль
+    print(random.randint(0, 1000))
